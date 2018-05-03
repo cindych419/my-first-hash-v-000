610806c4b932164e50d93139c5af67fc953c4db4
cindych419@gmail.com <cindych419@gmail.com>
2018-05-03 21:23:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,8 +36,9 @@ def adding
                  }
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  
+
   shipping_manifest
+end
 end
   # add 2 muskets to the shipping_manifest hash below
 
